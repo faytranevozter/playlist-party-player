f3835d06d7c4365a76a5194d54a0a5f78d696141
Fahrur Rifai <mfahrurrifai@gmail.com>
2024-03-15 19:55:16 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Player from './components/player/MainPlayer.vue'
+</script>
+
+<template>
+  <main class="bg-slate-500 min-h-dvh">
+    <div class="max-w-md mx-auto">
+      <Player />
+    </div>
+  </main>
+</template>
